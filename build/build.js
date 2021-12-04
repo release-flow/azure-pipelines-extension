b25a7c6ac434007725e46eb40d5bcb6ab8420063
Rich Tebb <rich@tebb.io>
2021-12-04 10:19:57 +0000

    Update build script

---
+++ build/build.js
@@ -0,0 +1,231 @@
+#!/usr/bin/env node
+
+const log = require('loglevel');
+const path = require('path');
+const del = require('del');
+const fs = require('fs');
+const fsPromises = fs.promises;
+const globby = require('globby');
+const spawn = require('child_process').spawn;
+const yargs = require('yargs');
+
+const argv = yargs(process.argv.slice(2))
+  .options({
+    major: {
+      default: 0,
+      type: 'number',
+    },
+    minor: {
+      default: 0,
+      type: 'number',
+    },
+    patch: {
+      default: 0,
+      type: 'number',
+    },
+    buildCounter: {
+      default: 0,
+      type: 'number',
+    },
+    buildType: {
+      default: 'local',
+      type: 'string',
+    },
+    preReleaseNumber: {
+      default: 1,
+      type: 'number',
+    },
+    preReleaseLabel: {
+      default: null,
+      type: 'string',
+    },
+    publisher: {
+      default: 'local',
+      type: 'string',
+    },
+    outputDir: {
+      default: 'dist/',
+      type: 'string',
+    },
+    serverBuild: {
+      type: 'boolean',
+      default: false,
+    },
+    verbose: {
+      type: 'boolean',
+      default: false,
+    },
+    clean: {
+      type: 'boolean',
+      default: false,
+    },
+  }).argv;
+
+argv.isPreRelease = !!argv.preReleaseLabel;
+
+function run(command) {
+  return new Promise((resolve, reject) => {
+    const cmd = spawn(command.cmd, command.args, {
+      shell: true,
+      stdio: 'inherit',
+      cwd: command.cwd || process.cwd(),
+    });
+    cmd.on('error', (err) => {
+      reject(err);
+    });
+    cmd.on('close', (code) => {
+      if (code !== 0) {
+        reject(new Error(`"${command.cmd}" exited with non-zero code ${code}`));
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+
+async function ensureOutputDir(dirName) {
+  const targetDir = path.resolve(outputDir, dirName);
+  await fsPromises.mkdir(targetDir, { recursive: true });
+}
+
+async function prepareOutputDir() {
+  if (argv.clean && fs.existsSync(argv.outputDir)) {
+    await del(path.join(argv.outputDir, '**'))
+  }
+
+  await fsPromises.mkdir(argv.outputDir, { recursive: true });
+}
+
+async function copyFilesToOutput(patterns, rootDir, targetDir) {
+  rootDir = rootDir || process.cwd();
+  targetDir = targetDir || argv.outputDir;
+  const entries = globby.stream(patterns, { cwd: rootDir, dot: true });
+  for await (const entry of entries) {
+    await ensureOutputDir(path.dirname(entry));
+    const source = path.join(rootDir, entry);
+    const target = path.join(targetDir, entry);
+    log.debug(`${source} -> ${target} (${path.dirname(entry)})`);
+    await fsPromises.copyFile(source, target);
+  }
+}
+
+function getExtensionSuffix() {
+  let suffix;
+  switch (argv.preReleaseLabel) {
+    case 'beta':
+      suffix = `${argv.major}-${argv.minor}-beta`;
+      break;
+
+    case null:
+    case '':
+      suffix = '';
+      break;
+
+    default:
+      suffix = argv.preReleaseLabel;
+      break;
+  }
+
+  return suffix;
+}
+
+
+/**
+ * Munges the extension manifest by setting the version number, etc.
+ *
+ */
+async function mungeExtensionManifest() {
+  const manifestFile = path.join(outputDir, 'vss-extension.json');
+  log.debug(`Munging extension manifest ${manifestFile}`);
+  let jsonText = await fsPromises.readFile(manifestFile);
+  let manifest = JSON.parse(jsonText);
+
+  manifest.version = `${argv.major}.${argv.minor}.${argv.patch}`;
+
+  if (argv.isPreRelease) {
+    // Add a suffix to the ID and name. This means each source branch will produce a different
+    // extension ID, to prevent version numbering clashes
+    const idSuffix = getExtensionSuffix();
+    manifest.id = `${manifest.id}-${idSuffix}`;
+    manifest.name = `${manifest.name} (${idSuffix})`;
+
+    log.debug(`Override extension id = ${manifest.id}`);
+    log.debug(`Override extension name = ${manifest.name}`);
+
+    // Increment the patch number with each build
+    const buildNum = (argv.preReleaseNumber * 1000) + argv.buildCounter
+    manifest.version += `.${buildNum}`;
+  }
+
+  log.debug(`Override extension version = ${manifest.version}`);
+
+  const data = JSON.stringify(manifest);
+  await fsPromises.writeFile(manifestFile, data);
+}
+
+async function copyExtensionFiles() {
+  await copyFilesToOutput(['**/*'], 'extension', outputDir);
+  await mungeExtensionManifest();
+}
+
+async function mungeTaskJsonFile() {
+
+}
+
+async function copyTaskFiles(taskName) {
+  // taskName is the name of the directory relative to tasks/
+  await ensureOutputDir(taskName);
+  const taskInputDir = path.join('tasks/', taskName);
+  const taskOutputDir = path.join(outputDir, taskName);
+
+  // Copy across task files
+  await copyFilesToOutput(['**/*.js', 'task.json', '!tests/**/*'], taskInputDir, taskOutputDir);
+
+  taskPatch = argv.major * 10000 + argv.minor * 100 + argv.patch;
+
+  log.debug(`Pre-release number = ${argv.preReleaseNumber}`);
+  if (argv.isPreRelease) {
+    // If pre-release we need to add another increment, to ensure that the patch version
+    // doesn't collide with a previous task number
+    taskPatch = (taskPatch * 10000) + (argv.preReleaseNumber * 100) + argv.buildNumber
+    log.debug(`Pre-release override of task patch to ${taskPatch}`);
+  } else {
+    $taskPatch = ($taskPatch * 100) + $BuildNumber
+    log.debug(`Override of task patch to ${taskPatch}`);
+  }
+}
+
+async function installNpm() {
+  // NPM install
+  await copyFilesToOutput(['package.json', 'package-lock.json']);
+  // Ensure node_modules in dist
+  const command = {
+    cmd: 'npm',
+    args: ['ci', '--production'],
+    cwd: outputDir
+  }
+  await run(command);
+}
+
+async function main() {
+  await prepareOutputDir();
+  await copyExtensionFiles();
+  await installNpm();
+  await copyTaskFiles('ReleaseFlowGitVersionV0');
+}
+
+/******************************************************************************
+ * MAIN LOGIC STARTS HERE
+ */
+log.setDefaultLevel('INFO');
+
+if (argv.verbose || (process.env.SYSTEM_DEBUG && process.env.SYSTEM_DEBUG.toLowerCase() === 'true')) {
+  log.setLevel('DEBUG');
+}
+
+log.debug(`CWD = ${process.cwd()}`);
+
+const outputDir = argv.outputDir;
+
+main();
--- extension/README.md
+++ extension/README.md
@@ -23,4 +23,40 @@ The [release-flow documentation](https://github.com/release-flow/release-flow#re
 
 ### Inputs
 
+| Name         | Description |
+| ------------ | ----------- |
+| `sourceBranch` | Specifies the branch on which the build is taking place, or from which the build was triggered, e.g. `$(Build.SourceBranch)`. |
+| `targetBranch` | If the build is triggered by a PR, specifies the target branch of the PR. |
+| `updateBuildNumber` | Indicates whether to update the build number to the calculated semantic version. |
+| `repoRoot` | Specifies the directory where the Git repository is located.  Leave this empty to use the root of the repo for the current build, which is `$(Build.SourcesDirectory)`. |
+
 ### Outputs
+
+| Name         | Description |
+| ------------ | ----------- |
+| `Major` | The major version number. |
+| `Minor` | The minor version number. |
+| `Patch` | The patch version number. |
+| `MajorMinor` | The major and minor version number, e.g. `1.2`. |
+| `MajorMinorPatch` | The major, minor, and patch version number, e.g. `1.2.3`. |
+| `SemVer` | The semantic version, e.g. `3.0.0-beta.1`. |
+| `LegacySemVer` | The legact (v1) semantic version, e.g. `3.0.0-beta1`. |
+| `AssemblySemVer` | The assembly version, e.g. `3.2.0.0`. |
+| `AssemblySemFileVer` | The assemby file version, e.g. `3.2.1.0`. |
+| `InformationalVersion` | The assembly informational version, e.g. `3.0.0-beta.1+1.Branch.release/3.0.0.Sha.28c853159a46b5a87e6cc9c4f6e940c59d6bc68a`. |
+| `BranchName` | The name of the branch or tag that triggered the build, e.g. `release/3.0`. |
+| `EscapedBranchName` | The branch name, escaped for SemVer compatibility e.g. `release-3.0`. |
+| `BuildType` | The build type that was inferred from the source branch and repository state. One of: `alpha`, `beta`, `working-branch`, `pull-request`, `release`. |
+| `Sha` | The Git hash of the commit that triggered the build. |
+| `ShortSha` | The abbreviated Git hash of the commit that triggered the build. |
+| `CommitDate` | The date and time of the commit that triggered the build in ISO 8601 standard format, e.g. `2020-02-07T15:52:51.000Z`. |
+| `CommitsSinceVersionSource` | The number of commits on the ancestry path between the current commit and the last version source. |
+| `CommitsSinceVersionSourcePadded` | Number of commits since version source padded to 4 digits, e.g. `0001`. |
+| `VersionSourceSha` | The Git hash of the commit that was identified as the version source. |
+| `PreReleaseLabel` | The pre-release label that was inferred from the source branch and repository state, e.g. `beta`, `feature-test-stuff`. |
+| `PreReleaseTag` | The NuGet pre-release tag, e.g. `feature-test-stuff0004`. |
+| `PreReleaseTagWithDash` | The NuGet pre-release tag, prefixed with a dash, e.g. `-feature-test-stuff0004`. |
+| `PreReleaseNumber` | The NuGet pre-release number, e.g. `4`. |
+| `FullBuildMetaData` | The full build metadata, e.g. `1.Branch.release/3.0.0.Sha.28c853159a46b5a87e6cc9c4f6e940c59d6bc68a`. |
+| `NuGetVersion` | The legacy NuGet-compatible version, e.g. `3.0.0-beta0001`. |
+| `NuGetPreReleaseTag` | The NuGet-compatible pre-release tag, e.g. `beta0001`. |
